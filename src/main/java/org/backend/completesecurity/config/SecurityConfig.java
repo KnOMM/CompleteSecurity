659d20ffa26d4574d4980b140cdb579feb36a34f
Nazar Zhuhan <nazarzhuhan@gmail.com>
2024-03-30 23:09:29 +0200

    Initial commit

---
+++ src/main/java/org/backend/completesecurity/config/SecurityConfig.java
@@ -0,0 +1,4 @@
+package org.backend.completesecurity.config;
+
+public class SecurityConfig {
+}
---
+++ src/test/java/org/backend/completesecurity/CompleteSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.backend.completesecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompleteSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
